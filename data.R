c05e5cd432ee9782c8ffd34e9662e6217eb067ca
Graydon "Cedric" Elkouh <gelkouh@uchicago.edu>
2021-01-07 09:56:41 -0500

    Add files via upload

---
+++ data.R
@@ -0,0 +1,3 @@
+ddir <- "/Users/gelkouh/Google Drive (UChicago)/EIA Cohort (Shared Folder)/data"
+# Change this file path to reflect the file path to where the `data` folder for this project is stored on your machine
+# You can download the `data` folder here: https://drive.google.com/drive/folders/1kZHpzNzSht9ov1M0jJPrRLT_KGFKH7Es?usp=sharing
