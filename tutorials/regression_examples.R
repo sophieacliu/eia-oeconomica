fe12b2fbb1954c180967180ae8731c28e94181a4
Graydon "Cedric" Elkouh <gelkouh@uchicago.edu>
2021-01-07 19:25:52 -0500

    Update regression_examples.R

--- tutorials/regression_examples.R
+++ tutorials/regression_examples.R
@@ -1,1 +1,4 @@
 # sample code for common regression techniques
+
+# Common regression types and when to use them (Regular Multiple Regression; Diff-in-Diff; Fixed Effects; IV; Logit)
+# Note on dummy variables for categorical data
